58ad9be15d673d063ce83692cb70eb2e6ba43457
Tremelle Ancrum <tancrum1996@gmail.com>
2022-06-24 15:27:09 -0400

    commit tree 3975f7b1d7b49c1026e45eeddb6655d8fa927593

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) { 
+if (pickUpLocation > 42) {
+    return pickUpLocation - 42
+  } else {
+  return 42 - pickUpLocation;
+  }
+}
+
+
+function distanceFromHqInFeet(pickUpLocation) {
+  return distanceFromHqInBlocks(pickUpLocation) * 264;
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+  return distanceFromHqInFeet(start) - distanceFromHqInFeet(destination)
+}
